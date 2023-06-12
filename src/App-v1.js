3d192d5912682f969c5e25b838c64de3c800fe50
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-12 22:00:59 +0500

    changed with children props

---
+++ src/App-v1.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const step = 1;
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(s => s - 1);
+  };
+
+  const handleNext = () => {
+    if (step < 3) {
+      setStep(s => s + 1);
+      // setStep(s => s + 1);
+    }
+  };
+
+  const handleClose = () => setIsOpen(is => !is);
+
+  return (
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
